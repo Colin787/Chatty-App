4c381665d5f37fb54ba80a27624f5792ab027a88
Colin787 <colinpark4@gmail.com>
2017-06-27 03:44:37 +0000

    added styles and completed the home page for step 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,27 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
-    );
+      <div>
+        <div>
+          <div>
+            <div>
+              <nav className="navbar">
+                <div className="navbar-brand">Chatty</div>
+              </nav>
+              <p><span className="message-username">Anon</span> random dhf asd  fas f afs  sf a fs f a sfa f sa f </p>
+            </div>
+          </div>
+         <div className="chatbar">
+            <input type="text" className="chatbar-username" placeholder="Your Name (Optional)"/>
+            <input type="text" className="chatbar-message" placeholder="Type a message and hit ENTER"/>
+         </div>
+        </div>
+      </div>
+
+
+    )
   }
 }
+
 export default App;
+
