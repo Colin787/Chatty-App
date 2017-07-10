3ffb19d7ee57203bc7810ad7e540517c3b8e8176
Colin787 <colinpark4@gmail.com>
2017-07-10 17:16:53 +0000

    added a comment and if statements to andle notifications/incomingMessages

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
 import Message from './Message.jsx';
+import Notification from './Notification.jsx'
 
 class MessageList extends Component {
   render() {
     return (
-      <div>
-        {this.props.messages.map((message) => {
-          return <Message key = {message.id} content = {message.content} username = {message.username}/>
-        })}
-
-      </div>
+      //handles the message/notification on the client side
+      <main className="messages">
+        {
+          this.props.messages.map((message, i) => {
+            if (message.type === 'incomingMessage'){
+              return <Message key={i} username={message.username} content={message.content}/>
+            }
+            if (message.type === 'incomingNotification') {
+              return <Notification key={i} newUser={message.newUser} oldUser={message.oldUser}/>
+            }
+          })
+        }
+    </main>
    );
   }
 }
