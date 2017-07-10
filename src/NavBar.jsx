4a162ad3c55fc99fb48658aa91f602874e0d1a3c
Colin787 <colinpark4@gmail.com>
2017-07-10 17:17:52 +0000

    now you can see how many users are connected

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 
+//Navbar that shows a count of online users
 class Navbar extends Component {
   render() {
     return (
     <nav className="navbar">
      <a href="/" className="navbar-brand">Chatty</a>
+     <h2 className="count">Online Users: {this.props.count} </h2>
     </nav>
    );
   }
