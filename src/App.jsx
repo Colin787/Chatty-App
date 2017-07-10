6c8c115bab72fc729de61dbef25bebf89ec0ac4d
Colin787 <colinpark4@gmail.com>
2017-07-10 17:12:23 +0000

    refactored code with names that made more sense and added comments

--- src/App.jsx
+++ src/App.jsx
@@ -8,42 +8,83 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: []
+      currentUser: { name: 'Bob' },
+      messages: [],
+      count: 0,
     };
+
+// binds the functions addNewMessage and userNotification
+
+    this.addNewMessage = this.addNewMessage.bind(this);
+    this.userNotification = this.userNotification.bind(this);
     this.socket = new WebSocket('ws://localhost:3001/');
-    this.socket.onopen = (e) => {
-      this.socket.onmessage = (event) => {
-      console.log(event);
-      const newMessage = JSON.parse(event.data);
-      const messages = this.state.messages.concat(newMessage);
-      this.setState({messages: messages});
-      }
-    }
   }
 
+//Add message function that sets the state and sends the message/username to the server from chat bar
+
   addNewMessage(username, content) {
-    const newMessage = {id: Date.now(), username: username, content: content};
-    this.socket.send(JSON.stringify(newMessage)); 
+    const newMessage = { type: "postMessage", username: username, content: content };
+    if (this.state.currentUser.name !== username) {
+      this.userNotification(username);
+    }
+    this.socket.send(JSON.stringify(newMessage))
   }
+
+//User notification function that sets the state and sends the new user and old user to the server
+
+  userNotification(newUser) {
+    const notification = { type: "postNotification", oldUser: this.state.currentUser.name, newUser: newUser };
+    this.setState({currentUser:{name: newUser}})
+    this.socket.send(JSON.stringify(notification))
+  }
+
+//Handles the server -> client handshake
+
+  componentDidMount() {
+    this.socket.onopen = (e) => {
+      console.log('Connected to server');
+    };
+
+//Handles the on message from the server and the different types of cases
+//Last life cycle before render
+
+    this.socket.onmessage = (e) => {
+      const data = JSON.parse(e.data);
+      switch (data.type) {
+        case "incomingMessage":
+          const messages = this.state.messages.concat(data);
+          this.setState({ messages: messages })
+          break;
+        case "incomingNotification":
+          const notifications = this.state.messages.concat(data);
+          this.setState({ messages: notifications })
+          break;
+        case "userCount":
+          const count = this.state.count
+          this.setState({count: data.count})
+          break;
+        default:
+          throw new Error("Unknown event type " + data.type);
+      }
+    };
+  };
   
-  userNotification (username) {
-    const notification = {type: "postNotification", oldUser: this.state.currentUser.name, newUser: newUser};
-    this.setState({currentUser:{name: newUser}});
-    this.socket.send(JSON.stringify(notification));
-    
+  componentDidUpdate() {
+    window.scrollTo(0, document.querySelector(".messages").scrollHeight);
   }
+
   render() {
     return (
+  
+//includes the state to allow each component to use them 
+
       <div>
-        <Navbar />
-        <MessageList messages = {this.state.messages}/>
-        <ChatBar currentUser={this.state.currentUser} userNotification={this.userNotification.bind(this)} newMessage={this.addNewMessage.bind(this)}/>
+        <ChatBar currentUser={this.state.currentUser} addNewMessage={this.addNewMessage} userNotification={this.userNotification} />
+        <MessageList messages={this.state.messages}/>
+        <Navbar count={this.state.count} />
       </div>
     );
   }
-
 }
-
 export default App;
 
